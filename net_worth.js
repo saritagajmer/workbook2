970fcd82c25b3239e8bfeb3d9c0b62ed75954414
Sarita Gajmer <saritagajmer36@gmail.com>
2023-10-18 10:43:31 -0400

    netWorth

--- net_worth.js
+++ net_worth.js
@@ -1,9 +1,8 @@
 
-var house = "25000";
-var car = "30000";
-var savings = "15000"
+var house = 25000;
+var car = 30000;
+var savings = 15000
  
-// var debt = "1000"
 
 var netWorth = house + car + savings;
-console.log(" Your netWorth is: $"+ netWorth);
+console.log(" Your netWorth is $"+ netWorth);
